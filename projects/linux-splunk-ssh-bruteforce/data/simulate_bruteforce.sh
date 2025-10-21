9c60f43502ba49037c4073ff661c6d85852466d7
Yurii Kholmohorov <hibiscus11114@gmail.com>
2025-10-21 17:44:45 +0400

    chore: add simulate_bruteforce.sh (LF, no BOM) and set exec bit

---
+++ projects/linux-splunk-ssh-bruteforce/data/simulate_bruteforce.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Usage: sudo bash simulate_bruteforce.sh <attempts> [ip]
+ATTEMPTS=${1:-20}
+IP=${2:-127.0.0.1}
+USER="invaliduser"
+
+for (( i=1; i<=ATTEMPTS; i++ )); do
+  MSG="sshd[$$]: Failed password for invalid user ${USER} from ${IP} port $((1024 + RANDOM % 40000)) ssh2"
+  logger -t sshd "$MSG"
+  sleep 0.2
+done
+
+echo "Simulated $ATTEMPTS failed SSH attempts from $IP"
